a28f5ee4d770d6492c772b940c6a521b62a23525
LegendBIT <48381534+LegendBIT@users.noreply.github.com>
2022-02-22 20:34:58 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -25,12 +25,12 @@ image = transform(image)
 image = image.reshape(1, 3, 128, 128)
 
 # 读取模型
-model = torch.load("./mobilenetv2-v0.1_20220120-2156_128_1.0_9_0.8886_0.8856.pth")
+model = torch.load("./mobilenetv2-v0.1_20220120-2156_128_1.0_9_0.8886_0.8856.pth")  # 并不能像tf一样直接导入，同一级目录下需要有模型的定义文件，这个不方便
 # print(model)
 
 # 单张图片推理
-model.cpu().eval()
-with torch.no_grad():
+model.cpu().eval()     # .eval()用于通知BN层和dropout层，采用推理模式而不是训练模式
+with torch.no_grad():  # torch.no_grad()用于整体修改模型中每一层的requires_grad属性，使得所有可训练参数不能修改，且正向计算时不保存中间过程，以节省内存
     output = torch.squeeze(model(image))
     predict = torch.softmax(output, dim=0)
     predict_cla = torch.argmax(predict).numpy()
